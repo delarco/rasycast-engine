021bfa8a6991540f7cccc0242cb52a6af9e179f4
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-08 14:12:12 -0300

    Create tile class and refactor map

--- src/core/Map.ts
+++ src/core/Map.ts
@@ -1,19 +1,30 @@
 import { Size } from "./Size";
+import { Tile } from "./Tile";
+import { Vec2D } from "./Vec2D";
 
 // Placeholder map
-export var Map = {
+const map = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+]
+
+export var tileMap = {
 
     size: new Size(10, 10),
-    tiles: [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    ]
+    tiles: map.map(t => new Tile(new Vec2D(), t == 1))
+}
+
+for (let y = 0; y < tileMap.size.height; y++) {
+    for (let x = 0; x < tileMap.size.width; x++) {
+        const tile = tileMap.tiles[y * tileMap.size.width + x];
+        [tile.position.x, tile.position.y] = [x, y];
+    }
 }
---
+++ src/core/Tile.ts
@@ -0,0 +1,6 @@
+import { Vec2D } from "./Vec2D";
+
+export class Tile {
+
+    constructor(public position: Vec2D, public solid: boolean) { }
+}
