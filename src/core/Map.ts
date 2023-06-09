fd863343bc3a71badff35260b65a24ac605dbe70
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-09 12:46:46 -0300

    Add wall texture to tile

--- src/core/Map.ts
+++ src/core/Map.ts
@@ -1,3 +1,4 @@
+import { TextureUtils } from "../utils/Texture.utils";
 import { Size } from "./Size";
 import { Tile } from "./Tile";
 import { Vec2D } from "./Vec2D";
@@ -5,10 +6,10 @@ import { Vec2D } from "./Vec2D";
 // Placeholder map
 const map = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 2, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 2, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 2, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 2, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -16,15 +17,32 @@ const map = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 ]
 
-export var tileMap = {
+export class Map {
 
-    size: new Size(10, 10),
-    tiles: map.map(t => new Tile(new Vec2D(), t == 1))
-}
+    constructor(
+        public name: string,
+        public size: Size,
+        public tiles: Array<Tile> = []) { }
 
-for (let y = 0; y < tileMap.size.height; y++) {
-    for (let x = 0; x < tileMap.size.width; x++) {
-        const tile = tileMap.tiles[y * tileMap.size.width + x];
-        [tile.position.x, tile.position.y] = [x, y];
+    public async load(): Promise<void> {
+
+        this.tiles = new Array<Tile>();
+
+        const wall = await TextureUtils.loadTexture('textures/ground-01.png')!;
+        const test = await TextureUtils.loadTexture('textures/debug.png')!;
+
+        for (let y = 0; y < this.size.height; y++) {
+            for (let x = 0; x < this.size.width; x++) {
+
+                const num = map[y * this.size.width + x];
+
+                const tile = new Tile(
+                    new Vec2D(x, y),
+                    num > 0,
+                    num == 1 ? wall : test
+                );
+                this.tiles.push(tile);
+            }
+        }
     }
 }
--- src/core/Tile.ts
+++ src/core/Tile.ts
@@ -1,6 +1,11 @@
+import { Texture } from "./Texture";
 import { Vec2D } from "./Vec2D";
 
 export class Tile {
 
-    constructor(public position: Vec2D, public solid: boolean) { }
+    constructor(
+        public position: Vec2D,
+        public solid: boolean,
+        public wall: Texture,
+    ) { }
 }
