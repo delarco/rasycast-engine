2ee796a265cfee9b91f51004789b8ce70c8de41c
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-13 00:40:32 -0300

    Initialize tile texture/details

--- src/core/Tile.ts
+++ src/core/Tile.ts
@@ -9,5 +9,30 @@ export class Tile {
         public solid: boolean,
         public texture?: { [key in Side]: Texture | null } | null,
         public detail?: { [key in Side]: Texture | null } | null,
-    ) { }
+    ) {
+
+        if(!texture) {
+
+            this.texture = {
+                [Side.NORTH]: null,
+                [Side.SOUTH]: null,
+                [Side.WEST]: null,
+                [Side.EAST]: null,
+                [Side.TOP]: null,
+                [Side.BOTTOM]: null,
+            };
+        }
+
+        if(!detail) {
+
+            this.detail = {
+                [Side.NORTH]: null,
+                [Side.SOUTH]: null,
+                [Side.WEST]: null,
+                [Side.EAST]: null,
+                [Side.TOP]: null,
+                [Side.BOTTOM]: null,
+            };
+        }
+    }
 }
